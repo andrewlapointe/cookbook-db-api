8116ed34c289cf3048501977aa6d94cbc35a727c
Andrew LaPointe <andrewrlapointe@gmail.com>
2025-02-24 12:07:08 -0700

    added debug message

--- utilities/index.js
+++ utilities/index.js
@@ -4,7 +4,7 @@ const Util = {};
 
 Util.checkJWTToken = (req, res, next) => {
     const token = req.headers['authorization'];
-
+    console.log(`AUTH TOKEN: ${token}`);
     if (token == null) return res.sendStatus(401);
 
     jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
