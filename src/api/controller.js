eaba481aa814d28594ebefcb0138627f234edac3
Andrew LaPointe <andrewrlapointe@gmail.com>
2025-02-24 11:47:12 -0700

    fixed auth check

--- src/api/controller.js
+++ src/api/controller.js
@@ -78,7 +78,7 @@ controller.getAllRecipes = async function () {
 };
 
 controller.getRecipeById = async function (req) {
-    const id = req.body.id;
+    const id = req.params.id;
     const data = await pool.query(sql.selectRecipeByID);
     return data;
 };
--- utilities/index.js
+++ utilities/index.js
@@ -4,7 +4,6 @@ const Util = {};
 
 Util.checkJWTToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
 
     if (token == null) return res.sendStatus(401);
 
