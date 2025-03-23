2d61ccbf11d4e3cf590e7ebf9746c61c04a9a664
Andrew LaPointe <andrewrlapointe@gmail.com>
2025-03-22 18:27:03 -0600

    update

--- utilities/index.js
+++ utilities/index.js
@@ -4,6 +4,7 @@ const Util = {};
 
 Util.checkJWTToken = (req, res, next) => {
     const token = req.headers['authorization'];
+    console.log(token);
     if (token == null) return res.sendStatus(401);
 
     jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
